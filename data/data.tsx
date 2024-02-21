cf0be1e4da517e7ab4beb8ba600fe0cbde111b1e
ilBuso <alessandro.busola@gmail.com>
2024-02-21 15:45:59 +0100

    Update data.tsx

--- data/data.tsx
+++ data/data.tsx
@@ -64,7 +64,7 @@ export const DATA = {
             "Java",
             "SQL",
             "MongoDB",
-            "NextJS"
+            "NextJS",
             "Unity - GameEngine"
         ]
     },
